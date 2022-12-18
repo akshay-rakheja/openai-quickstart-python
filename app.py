192123c3fbb50cb7867f23b268e77ea9168c52fc
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-12-17 20:05:24 -0500

    feat: initial commit

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/", methods=("GET", "POST"))
-def index():
+def bob():
     if request.method == "POST":
         animal = request.form["animal"]
         response = openai.Completion.create(
-            model="text-davinci-002",
+            model="text-davinci-003",
             prompt=generate_prompt(animal),
             temperature=0.6,
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        return redirect(url_for("bob", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
